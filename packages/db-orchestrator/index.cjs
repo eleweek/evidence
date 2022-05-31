1bb5aa9bd18b0583cabdca090bd14b55d0d53886
Udesh Senaratne <usenaratne@gmail.com>
2022-05-30 21:57:44 -0400

    Updated query cache to store the column names

--- packages/db-orchestrator/index.cjs
+++ packages/db-orchestrator/index.cjs
@@ -5,6 +5,8 @@ const logEvent = require('@evidence-dev/telemetry')
 //const getColumnSummary = require('@evidence-dev/components/modules/getColumnSummary');
 const readline = require('readline');
 
+const cacheDirectory = "./.evidence-queries/cache";
+
 var EvidenceType;
 (function (EvidenceType) {
     EvidenceType["BOOLEAN"] = "boolean";
@@ -20,31 +22,27 @@ var TypeFidelity;
     TypeFidelity["UNKNOWN"] = "unknown";
 })(TypeFidelity || (TypeFidelity = {}));
 
-const getCache = function (dev, queryString, queryTime) {
-    queryTime = md5(queryTime)
-    if (dev) {
-        const cache = readJSONSync("./.evidence-queries/cache/" + queryTime + "/" + md5(queryString) + ".json", { throws: false })
-        if (cache) {
-            logEvent("cache-query", dev)
-            return cache
-        }
+
+const getQueryCachePaths = (queryString, queryTime) => {
+    let path = `${cacheDirectory}/${md5(queryTime)}`;
+    return {
+        'cacheDirectory': path,
+        'resultsCacheFile': `${cacheDirectory}/${md5(queryTime)}/${md5(queryString)}.json`,
+        'columnTypeCacheFile': `${cacheDirectory}/${md5(queryTime)}/${md5(queryString)}-column-types.json`,
     }
 }
 
-const updateCache = function (dev, queryString, data, queryTime) {
-    queryTime = md5(queryTime)
+const updateCache = function (dev, queryString, data, columnTypes, queryTime) {
     if (dev) {
-        if (!pathExistsSync("./.evidence-queries")) {
-            mkdirSync("./.evidence-queries")
+        const {cacheDirectory, resultsCacheFile, columnTypeCacheFile} = getQueryCachePaths(queryString, queryTime);
+        if (!pathExistsSync(cacheDirectory)) {
+            emptyDirSync(cacheDirectory); //TODO I think this invalidates the entire cache.  Check if this is by design.
+            mkdirSync(cacheDirectory, { recursive: true });
         }
-        if (!pathExistsSync("./.evidence-queries/cache/")) {
-            mkdirSync("./.evidence-queries/cache/")
+        writeJSONSync(resultsCacheFile, data, { throws: false });
+        if (columnTypes) {
+            writeJSONSync(columnTypeCacheFile, data, { throws: false });
         }
-        if (!pathExistsSync("./.evidence-queries/cache/" + queryTime)) {
-            emptyDirSync('./.evidence-queries/cache/')
-            mkdirSync("./.evidence-queries/cache/" + queryTime)
-        }
-        writeJSONSync("./.evidence-queries/cache/" + queryTime + "/" + md5(queryString) + ".json", data, { throws: false })
     }
 }
 
@@ -124,6 +122,16 @@ const processQueryResults = function (queryResults) {
     
     return {rows, fieldTypes};
 }
+
+const setQueryColumnTypes = (data, queryIndex, columnTypes) => {
+    let queryMetaData = data.evidencemeta?.queries[queryIndex];
+    if (columnTypes) {
+        queryMetaData.fieldTypes = columnTypes;
+    } else {
+        console.log("Field types not found");
+    }
+} 
+
 const runQueries = async function (routeHash, dev) {
     const settings = readJSONSync('./evidence.settings.json', {throws:false})
     const runQuery = await importDBAdapter(settings)
@@ -139,26 +147,35 @@ const runQueries = async function (routeHash, dev) {
         for (let queryIndex in queries) {
             let query = queries[queryIndex];
             let queryTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), new Date().getHours());              
-            let cache = undefined;//getCache(dev, query.compiledQueryString, queryTime)
+            let cache, columnTypeCache;
+            if (dev) {
+                const { resultsCacheFile, columnTypeCacheFile } = getQueryCachePaths(query.compiledQueryString, queryTime);
+                cache = readJSONSync(resultsCacheFile, { throws: false });
+                columnTypeCache = readJSONSync(columnTypeCacheFile, { throws: false });
+            }
             if (cache) {
-                data[query.id] = cache
-                process.stdout.write(chalk.greenBright("✓ "+ query.id) +  chalk.grey(" from cache \n"))
+                logEvent("cache-query", dev);
+                data[query.id] = cache;
+                if (columnTypeCache) {
+                    setQueryColumnTypes(data, queryIndex, columnTypeCache);
+                }
+                process.stdout.write(chalk.greenBright("✓ "+ query.id) +  chalk.grey(" from cache \n"));
             } else {
                 try {
                     process.stdout.write(chalk.grey("  "+ query.id +" running..."));
-                    validateQuery(query)
-                    let queryResults = processQueryResults(await runQuery(query.compiledQueryString, settings?.credentials, dev));
-                    console.log(`Ran query ${query.id} and obtained queryResults ${JSON.stringify(queryResults, null, 2)}`);
-                    data[query.id] = queryResults.rows;
-                    if (queryResults.fieldTypes) {
-                        let queryMetaData = data.evidencemeta?.queries[queryIndex];
-                        queryMetaData.fieldTypes = queryResults.fieldTypes;
-                    } else {
-                        console.log("Field types not found");
-                    }
+                    validateQuery(query);
+                    let {rows, fieldTypes} = processQueryResults(await runQuery(query.compiledQueryString, settings?.credentials, dev));
+
+                    console.log(`Ran query ${query.id} and obtained rows ${JSON.stringify(rows, null, 2)} and columnTypes=${JSON.stringify(fieldTypes, null, 2)}`);
+
+                    data[query.id] = rows;
+                    setQueryColumnTypes(data, queryIndex, fieldTypes);
+
                     readline.cursorTo(process.stdout, 0);
                     process.stdout.write(chalk.greenBright("✓ "+ query.id) + chalk.grey(" from database \n"))
-                    updateCache(dev, query.compiledQueryString, data[query.id], queryTime)
+
+                    updateCache(dev, query.compiledQueryString, data[query.id], fieldTypes, queryTime);
+
                     logEvent("db-query", dev, settings)
                 } catch(err) {
                     readline.cursorTo(process.stdout, 0);
